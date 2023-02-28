0f243f8e6bc4b949842a257b9942d36055c7dbf8
minktruong1 <minhpro207@yahoo.com>
2023-02-28 19:59:32 +0700

    Card and card-flip animation

--- src/components/InteractScreen.vue
+++ src/components/InteractScreen.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>Interact Screen</h1>
+  <card-flip />
 </template>
+
+<script>
+import CardFlip from "./CardView.vue";
+
+export default {
+  components: {
+    CardFlip,
+  },
+};
+</script>
