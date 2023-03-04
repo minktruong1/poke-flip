8119b61079d58614c1be6f0c43281f7352be4f6f
minktruong1 <114055197+minktruong1@users.noreply.github.com>
2023-03-04 14:39:03 +0700

    Update MainScreen.vue

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -3,7 +3,7 @@
     <h1>POKE FLIP</h1>
     <h2>Select mode to start</h2>
     <div class="modes">
-      <button @click="onStart(4)">
+      <button @click="onStart(16)">
         <span>4x4</span>
         <span>Easy</span>
       </button>
